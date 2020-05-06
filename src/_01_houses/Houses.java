5f03ee747a277c7feb5f6013d4fbcb2322182fd9
cfredberg <53453343+cfredberg@users.noreply.github.com>
2020-05-06 16:58:08 -0700

    Houses IP

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,32 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	static Robot robo = new Robot();
+	
 	public void run() {
-		
+		robo.moveTo(10, 500);
+		robo.penDown();
+		robo.setAngle(90);
+		house("small");
+	}
+	
+	public static void house(String size) {
+		if (size.equals("small")) {
+			robo.turn(-90);
+			robo.move(60);
+			pointyRoof();
+			robo.move(60);
+			robo.turn(-90);
+		}
+	}
+	
+	public static void pointyRoof() {
+		robo.turn(30);
+		robo.move(20);
+		robo.turn(120);
+		robo.move(20);
+		robo.turn(30);
 	}
 }
